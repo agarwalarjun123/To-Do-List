90447f63980c2437cfd810cb269b01c2ef5f0a50
arjun <arjunagarwal1998@gmail.com>
2018-02-07 13:02:34 +0530

    node-to-do

--- index.js
+++ index.js
@@ -6,12 +6,9 @@ const get=require('./routes/get');
 const add=require('./routes/add');
 const del=require('./routes/del');
 const app=express();
-var mongoPassword = 'Arjun@1234';
-var config = JSON.parse(process.env.APP_CONFIG);
 app.set('etag', false);
 app.use(morgan('dev'));
-const url= "mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" + 
-    config.mongo.hostString;
+const url="mongodb://6a.mongo.evennode.com:27017,6b.mongo.evennode.com:27017/a2301f7599130336615033f7ce624d5e?replicaSet=eu-6 -u a2301f7599130336615033f7ce624d5e -p Arjun@1234";
 mongoose.connect(url,function(err,db){
 if(err)
 	console.log(err);
@@ -26,6 +23,6 @@ app.use('/get',get);
 app.use('/add',add);
 app.use('/delete',del);
 const server=http.createServer(app);
-server.listen(process.env.PORT,'0.0.0.0',function(){
+server.listen(3000,'localhost',function(){
 console.log("server running at port:3000");
 });
