312fbbdecd4ac00c23d84432afd9bd687cf4e554
arjun <arjunagarwal1998@gmail.com>
2018-02-07 13:10:28 +0530

    node-to-do

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const del=require('./routes/del');
 const app=express();
 app.set('etag', false);
 app.use(morgan('dev'));
-const url="mongodb://6a.mongo.evennode.com:27017,6b.mongo.evennode.com:27017/a2301f7599130336615033f7ce624d5e?replicaSet=eu-6 -u a2301f7599130336615033f7ce624d5e -p 12345678/conFusion";
+const url="mongodb://6a.mongo.evennode.com:27017,6b.mongo.evennode.com:27017/a2301f7599130336615033f7ce624d5e?replicaSet=eu-6 -u a2301f7599130336615033f7ce624d5e -p "+encodeURIComponent("12345678")+"/conFusion";
 mongoose.connect(url,function(err,db){
 if(err)
 	console.log(err);
